5e49fb7f62007c04ed5f17176efe0d4e49bb86dd
suubinkim <0vbbbv0@gmail.com>
2022-03-14 11:58:08 +0900

    빈 생명주기 콜백 방법2 - 빈등록 메서드

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -30,15 +30,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     //의존관계 주입 완료 후 실행 메소드
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         connect();
         call("초기화 연결 메세지");
     }
 
     //컨테이너 종료시 호출
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         disconnect();
     }
 }
