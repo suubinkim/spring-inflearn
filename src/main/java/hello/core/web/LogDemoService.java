8ae490c53f7618b429f5b09652e0742c5efe91c0
suubinkim <0vbbbv0@gmail.com>
2022-03-15 12:43:05 +0900

    request 스코프 예제 + 프록시

--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -2,18 +2,15 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+    private final MyLogger myLogger;
 
     public void logic(String testId) {
-        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.log("service id = " + testId);
     }
 }
